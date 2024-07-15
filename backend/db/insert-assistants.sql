dc4dbb838a5d019d429636461624066d21dd14f3
fedemelo <f.melo@uniandes.edu.co>
2024-07-14 21:34:53 -0500

    Scripts to insert assistants and their availabilites

---
+++ backend/db/insert-assistants.sql
@@ -0,0 +1,12 @@
+-- insert-assistants.sql
+
+-- Execute with `psql -U postgres -d cupihorarios -f insert-assistants.sql`
+
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (202021525, 'f.melo', 'Federico', 'Melo Barrero', 'Fede');
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (202011140, 'm.ruizg', 'Mariana', 'Ruiz Giraldo', 'Maru');
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (202112020, 's.martinezn', 'Santiago', 'Martinez Novoa', 'Santi');
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (201913554, 'am.ochoat', 'Andres Martin', 'Ochoa Toro', NULL);
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (201820147, 'ce.rozob', 'Camilo Esteban', 'Rozo Benitez', NULL);
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (201922019, 'n.carvajalc', 'Nicolas', 'Carvajal Chaves', 'Nico');
+-- INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (202021113, 'd.fuquen', 'David Alejandro', 'Fuquen Florez', 'Dave');
+INSERT INTO assistants (code, login, first_names, last_names, nickname) VALUES (194821525, 'm.laserna', 'Mario Alberto', 'Laserna Pinzon', NULL);
