5c50aaf3b415ffbb464ca436566daa860a13e1f2
SantiagoM99 <88895769+SantiagoM99@users.noreply.github.com>
2024-08-02 05:49:58 -0400

    erased changes

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,14 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import ScheduleTable from "../components/ScheduleTable";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
-  },
-  {
-    path: "/schedule",
-    element: <ScheduleTable />,
   },
 ]);
